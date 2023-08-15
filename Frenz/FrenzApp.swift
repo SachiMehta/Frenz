d622e3c22aeba00115c63a7a7ef7cb5956f06576
Sachi Mehta <sachimehta16@gmail.com>
2023-08-15 12:58:27 -0400

    Initial Commit

---
+++ Frenz/FrenzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenzApp.swift
+//  Frenz
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FrenzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
